61093e74de82e62b8f3643db5f3deef5a94541b6
Talal Najam <chelseatalal@gmail.com>
2019-01-10 05:40:12 +0500

    Updates

---
+++ precache-manifest.215468a638652d2810c3e072f6270fe5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "584b607f5c2ff075429dc0e7b8d142ef",
+    "url": "/movie-review/static/media/spinner2.584b607f.gif"
+  },
+  {
+    "revision": "6a13d08ea44514c475e6",
+    "url": "/movie-review/static/js/runtime~main.6a13d08e.js"
+  },
+  {
+    "revision": "9c61f34d78ff7417282c",
+    "url": "/movie-review/static/js/main.9c61f34d.chunk.js"
+  },
+  {
+    "revision": "c511377187a4ae813ae6",
+    "url": "/movie-review/static/js/1.c5113771.chunk.js"
+  },
+  {
+    "revision": "9c61f34d78ff7417282c",
+    "url": "/movie-review/static/css/main.b25abdcf.chunk.css"
+  },
+  {
+    "revision": "13f916a38f26b425d47f80199534706a",
+    "url": "/movie-review/index.html"
+  }
+];
